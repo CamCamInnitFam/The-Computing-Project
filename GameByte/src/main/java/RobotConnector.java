9f5db2d475fb2f106f3464db9b0da65d76da7c67
Cameron Crook <ccrook8@hotmail.co.uk>
2025-03-21 12:02:54 +0000

    small changes

--- GameByte/src/main/java/RobotConnector.java
+++ GameByte/src/main/java/RobotConnector.java
@@ -1,26 +1,45 @@
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.net.http.HttpClientService;
 
 import java.util.HashMap;
 import java.util.Map;
-public class RobotConnector {
+public class RobotConnector extends GameApplication {
 
-    public void takePicture(){
+    public static void takePicture(){
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "text/plain; charset=UTF-8");
-        headers.put("X-Tritium-Auth-Token", "");
+        headers.put("X-Tritium-Auth-Token", "hALeFOM4uzqpK6KWSr6ADY5Ou9MNXd");
         String text = "";
+
         FXGL.getTaskService().runAsync(FXGL.getService(HttpClientService.class)
-                .sendPUTRequestTask("https://rt-0143.robothespian.co.uk/"
+                .sendPUTRequestTask("https://rt-0143.robothespian.co.uk/tritium/text_to_speech/say"
                         ,text,
                         headers)
                 .onSuccess(resp -> {
-
+                    System.out.println("Success! " + resp);
                 })
                 .onFailure(e -> {
-
+                    System.out.println("Fail! " + e);
                 }));
     }
     public void savePicture(){}
     public void sendImage(){}
+    //M:\GameByte Novel Compression Solution\GameByte\src\main\resources\assets\textures\
+
+    public static void main(String[] args) {
+        //takePicture();
+        launch(args);
+    }
+
+    @Override
+    protected void initSettings(GameSettings gameSettings) {
+        gameSettings.addEngineService(HttpClientService.class);
+    }
+
+    @Override
+    protected void initGame() {
+        takePicture();
+    }
 }
