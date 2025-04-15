ecb794c3ea03062419bce0fbf63a06f10c2b3308
Cameron Crook <ccrook8@hotmail.co.uk>
2025-04-15 22:00:55 +0100

    Removed legacy code

--- GameByte/src/main/java/RobotConnector.java
+++ GameByte/src/main/java/RobotConnector.java
@@ -9,29 +9,7 @@ import java.util.HashMap;
 import java.util.Map;
 public class RobotConnector extends GameApplication {
 
-    public static void takePicture(){
-        Map<String, String> headers = new HashMap<>();
-        headers.put("Content-Type", "text/plain; charset=UTF-8");
-        headers.put("X-Tritium-Auth-Token", "hALeFOM4uzqpK6KWSr6ADY5Ou9MNXd");
-        String text = "";
-
-        FXGL.getTaskService().runAsync(FXGL.getService(HttpClientService.class)
-                .sendPUTRequestTask("https://rt-0143.robothespian.co.uk/tritium/text_to_speech/say"
-                        ,text,
-                        headers)
-                .onSuccess(resp -> {
-                    System.out.println("Success! " + resp);
-                })
-                .onFailure(e -> {
-                    System.out.println("Fail! " + e);
-                }));
-    }
-    public void savePicture(){}
-    public void sendImage(){}
-    //M:\GameByte Novel Compression Solution\GameByte\src\main\resources\assets\textures\
-
     public static void main(String[] args) {
-        //takePicture();
         launch(args);
     }
 
@@ -42,14 +20,15 @@ public class RobotConnector extends GameApplication {
 
     @Override
     protected void initGame() {
-        //takePicture();
         String imagePath = "E:\\Uni work\\Computing Project\\The-Computing-Project\\GameByte\\src\\main\\resources\\assets\\textures\\RobotTest.jpg";
         captureImageToFile(Path.of(imagePath));
     }
 
     public void captureImageToFile(Path file)
     {
-        FXGL.getService(HttpClientService.class).sendGETRequestTask("https://rt-0143.robothespian.co.uk/tritium/video_capture/jpeg", Map.of("X-Tritium-Auth-Token", getKey()),
+        FXGL.getService(HttpClientService.class)
+                .sendGETRequestTask("https://rt-0143.robothespian.co.uk/tritium/video_capture/jpeg",
+                        Map.of("X-Tritium-Auth-Token", getKey()),
                 HttpResponse.BodyHandlers.ofFile(file))
                 .onSuccess(res -> System.out.println(res.statusCode()))
                 .onFailure(e->{
